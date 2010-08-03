dec8752b20624ab85ce001a7498ab246953e1cfd
Raeez Lorgat <raeez@mit.edu>
2010-08-03 18:00:27 -0400

    Introduced TypeError and verbose output - now displays list of errors caught and raises single Strict::TypeError¬

--- Rakefile
+++ Rakefile
@@ -1,12 +1,15 @@
-# -*- ruby -*-
-
 require 'rubygems'
 require 'hoe'
+require 'lib/strict'
 
-Hoe.spec 'strict' do
-  developer('Raeez Lorgat', 'raeez@mit.edu')
-
-  # self.rubyforge_name = 'strictx' # if different than 'strict'
+Hoe.new('Strict', Strict::VERSION) do |p|
+  p.name = 'strict'
+  p.author = 'Raeez Lorgat'
+  p.description = 'Static typesystem for ruby'
+  p.email = 'raeez@mit.edu'
+  p.summary = 'Strict implements a static typesystem over dynamic ruby objects'
+  p.url = 'http://www.raeez.com/strict'
 end
 
-# vim: syntax=ruby
+desc "Release and publish documentation"
+task :repubdoc => [:release, :publish_docs]
--- lib/strict.rb
+++ lib/strict.rb
@@ -1,102 +1,159 @@
 module Strict
-  VERSION = '0.0.1'
+  VERSION = '0.0.3'
 
-  def enforce_primitive!(type, data)
-    raise "#{data} must be of type #{type}" unless (data.is_a? type and type.is_a? Class)
-  end
+  class TypeError < Exception
+    attr :errors
+    def initialize(error_list)
+      enforce!(:string_array, error_list, 'lib/strict')
+      @errors = error_list
+    end
 
-  def enforce_strict_primitives!(types, data)
-    enforce_primitive!(Array, types)
-    types.each {|type| enforce_primitive!(Object, type)}
-    types.each do |type|
-      enforce_primitive!(type, data)
+    def inspect
+      msg = "TypeError#{@errors.size > 1 ? "s" : ""} caught:\n"
+      @errors.each do |e|
+        msg += "#{e}\n"
+      end
+      return msg
     end
   end
 
-  def enforce_weak_primitives!(types, data)
-    enforce_primitive!(Array, types)
-    types.each {|type| enforce_primitive!(Object, type)}
+  @@errors = []
+  @@raise_exception = true
+
+  def setmode_raise!
+    @@errors = []
+    @@raise_exception = true
+  end
+
+  def setmode_catch!
+    @@errors = []
+    @@raise_exception = false
+  end
+
+  def catch_error error
+    @@errors << error
+    t = TypeError.new([error])
+    raise(t, t.inspect, caller) if @@raise_exception
+  end
+
+  def raise_hell!
+    t = TypeError.new(@@errors)
+    raise(t, t.inspect, caller) if @@errors.size > 0
+  end
+
+  def header(context, data)
+    "#{context}; #{data.class.inspect}::#{data.inspect}"
+  end
+
+  def enforce_primitive!(type, data, context="Value")
+    catch_error "#{header(context, data)} must be of type #{type}" unless (data.is_a? type and type.is_a? Class)
+    return data
+  end
+
+  def enforce_strict_primitives!(types, data, context="Value")
+    enforce_primitive!(Array, types, 'lib/strict')
+    types.each {|type| enforce_primitive!(Object, type, 'lib/strict')}
+    types.each do |type|
+      enforce_primitive!(type, data, context)
+    end
+    return data
+  end
+
+  def enforce_weak_primitives!(types, data, context="Value")
+    enforce_primitive!(Array, types, 'lib/strict')
+    types.each {|type| enforce_primitive!(Object, type, 'lib/strict')}
     
     match_found = false
     types.each do |type|
       begin
-        enforce_primitive!(type, data)
-        match_found = true
+        s = @@errors.size
+        enforce_primitive!(type, data, context)
+        match_found = true unless s != @@errors.size
       rescue
         next
       end
     end
-    raise "#{data} must be of one of the types of #{types.inspect}" unless match_found
+    catch_error "#{header(context, data)} must be of one of the types of #{types.inspect}" unless match_found
+    return data
   end
 
-  def enforce_non_nil!(obj)
-    raise "Object is nil!" if obj.nil?
+  def enforce_non_nil!(obj, context="Value")
+    catch_error "#{context}: Object is nil!" if obj.nil?
+    return obj
   end
 
-  def enforce!(supertype, data) #write a more generic handler, for new types?
-    enforce_non_nil!(data)
+  def enforce!(supertype, data, context="Value") #write a more generic handler, for new types?
+    enforce_non_nil!(data, context)
     
     if supertype.is_a? Array #enumeration of values
-      raise "#{data} should take on a value within #{supertype.inspect}" unless supertype.include? data
+      catch_error "#{header(context, data)} should take on a value within #{supertype.inspect}" unless supertype.include? data
 
     else if supertype.is_a? Symbol #distinct type
       case supertype
         when :string
-          enforce_primitive!(String, data)
-          raise "#{data} can't be empty string" unless (data.size > 0)
+          enforce_primitive!(String, data, context)
+          catch_error "#{header(context, data)} can't be empty string" unless (data.size > 0)
 
         when :hex_color
-          enforce!(:string, data)
-          raise "#{data} must be six characters long" unless (data.size == 6)
-          data.upcase.each_byte {|c| raise "#{data} must contain only hexadecimal characters" unless ((48 <= c  and c <= 57) or (65 <= c and c <= 70))}
+          enforce!(:string, data, context)
+          catch_error "#{header(context, data)} must be six characters long" unless (data.size == 6)
+          data.upcase.each_byte {|c| catch_error "#{header(context, data)} must contain only hexadecimal characters" unless ((48 <= c  and c <= 57) or (65 <= c and c <= 70))}
 
         when :natural_number
-          enforce_primitive!(Fixnum, data)
-          raise "#{data} must be > 0" unless (data > 0)
+          enforce_primitive!(Fixnum, data, context)
+          catch_error "#{header(context, data)} must be > 0" unless (data > 0)
 
         when :integer
-          enforce_primitive!(Fixnum, data)
+          enforce_primitive!(Fixnum, data, context)
 
         when :numeric
-          enforce_primitive!(Numeric, data)
+          enforce_primitive!(Numeric, data, context)
 
         when :boolean
-          enforce_weak_primitives!([TrueClass, FalseClass], data)
+          enforce_weak_primitives!([TrueClass, FalseClass], data, context)
 
         when :float
-          enforce_primitive!(Float, data)
+          enforce_primitive!(Float, data, context)
 
         when :string_array
-          enforce_primitive!(Array, data)
-          data.each {|item| enforce_primitive!(String, item)}
+          enforce_primitive!(Array, data, context)
+          data.each {|item| enforce_primitive!(String, item, context)}
 
         when :numeric_array
-          enforce_primitive!(Array, data)
-          data.each {|item| enforce_primitive!(Numeric, item)}
+          enforce_primitive!(Array, data, context)
+          data.each {|item| enforce_primitive!(Numeric, item, context)}
 
         when :float_array
-          enforce_primitive!(Array, data)
-          data.each {|item| enforce_primitive!(Float, item)}
+          enforce_primitive!(Array, data, context)
+          data.each {|item| enforce_primitive!(Float, item, context)}
 
         when :integer_array
-          enforce_primitive!(Array, data)
-          data.each {|item| enforce_primitive!(Fixnum, item)}
+          enforce_primitive!(Array, data, context)
+          data.each {|item| enforce_primitive!(Fixnum, item, context)}
 
         else
-          raise "undefined symbol-supertype encountered: #{supertype.inspect}"
+          catch_error "undefined symbol-supertype encountered: #{supertype.inspect}"
         end
       end
     end
+    return data
   end
 
-  def enforce_map!(requirements, args)
-    enforce_primitive!(Hash, requirements)
-    requirements.each do |param, type|
-      if args.has_key? param
-        enforce!(type, args[param])
+  def enforce_map!(matrix, map)
+    enforce_primitive!(Hash, matrix, "lib/strict")
+    enforce_primitive!(Hash, map, "lib/strict")
+
+    setmode_catch!
+
+    matrix.each do |param, type|
+      if map.has_key? param
+        enforce!(type, map[param], "map[#{param.inspect}]")
       else
-        raise "missing required param: #{param}"
+        catch_error "map: missing required param: #{param.inspect}"
       end
     end
+    raise_hell!
+    setmode_raise!
+    return map
   end
 end
